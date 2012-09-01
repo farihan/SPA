2b937d5fc52aa15dfa3661cb45620cce2bb5fb6e
Muhamad Farihan <farihan@gmail.com>
2012-09-02 02:04:48 +0800

    - add Product/Index.cshtml

--- src/Hans.SinglePageApp.Web/Controllers/ProductController.cs
+++ src/Hans.SinglePageApp.Web/Controllers/ProductController.cs
@@ -1,35 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Web.Http;
-using Hans.SinglePageApp.Core.Domains;
-using Hans.SinglePageApp.Core.Repositories;
-using Hans.SinglePageApp.Web.Models;
+using System.Web;
+using System.Web.Mvc;
 
 namespace Hans.SinglePageApp.Web.Controllers
 {
-    public class ProductController : ApiController
+    public class ProductController : Controller
     {
-        public IRepository<Product> ProductRepository { get; set; }
+        //
+        // GET: /Product/
 
-        public IQueryable<ProductModel> GetProducts()
+        public ActionResult Index()
         {
-            var list = ProductRepository.FindAll().Select(x => new ProductModel{
-                ProductID = x.ProductID,
-                ProductName = x.ProductName,
-                SupplierId = x.Supplier.SupplierID,
-                CategoryId = x.Category.CategoryID,
-                QuantityPerUnit = x.QuantityPerUnit,
-                UnitPrice = x.UnitPrice,
-                UnitsInStock = x.UnitsInStock,
-                UnitsOnOrder = x.UnitsOnOrder,
-                ReorderLevel = x.ReorderLevel,
-                Discontinued = x.Discontinued
-            });
-
-            return list;
+            return View();
         }
+
     }
 }
---
+++ src/Hans.SinglePageApp.Web/Controllers/ProductServiceController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Hans.SinglePageApp.Core.Domains;
+using Hans.SinglePageApp.Core.Repositories;
+using Hans.SinglePageApp.Web.Models;
+
+namespace Hans.SinglePageApp.Web.Controllers
+{
+    public class ProductServiceController : ApiController
+    {
+        public IRepository<Product> ProductRepository { get; set; }
+
+        public IQueryable<ProductModel> GetProducts()
+        {
+            var list = ProductRepository.FindAll().Select(x => new ProductModel{
+                ProductID = x.ProductID,
+                ProductName = x.ProductName,
+                SupplierId = x.Supplier.SupplierID,
+                CategoryId = x.Category.CategoryID,
+                QuantityPerUnit = x.QuantityPerUnit,
+                UnitPrice = x.UnitPrice,
+                UnitsInStock = x.UnitsInStock,
+                UnitsOnOrder = x.UnitsOnOrder,
+                ReorderLevel = x.ReorderLevel,
+                Discontinued = x.Discontinued
+            });
+
+            return list;
+        }
+    }
+}
